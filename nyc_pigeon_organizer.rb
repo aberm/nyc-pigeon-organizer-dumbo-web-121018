e44b680cd1bf72b9afa04ddd44c66640d750ef46
Menachem Aber <abermendel@gmail.com>
2018-12-02 18:21:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  new_data = {}
+  names = []
+  
+  data.each do |attribute, info|
+    # binding.pry
+    info.each do |key, value|
+      # binding.pry
+      i = 0
+      while i < value.length
+        if names.include?(value[i]) == false
+          names << value[i]
+        end
+      end
+    end
+  end
+  new_data
 end
