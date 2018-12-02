1ade50cde2d91b54459bfceefa9a99297738e48c
Menachem Aber <abermendel@gmail.com>
2018-12-02 18:37:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,48 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeon_list = {}
+  names = []
+  
+  data.each do |attribute, info|
+    info.each do |key, value|
+      i = 0
+      while i < value.length
+        if names.include?(value[i]) == false
+          names << value[i]
+          i += 1
+        else
+          i += 1
+        end
+      end
+    end
+  end
+  
+  names.each do |name|
+    pigeon_list[name] = {}
+  end
+  
+  x = 0
+  while x < names.length
+    
+    data.each do |attribute, info|
+      info.each do |key, value|
+        y = 0
+        while y < value.length
+        binding.pry
+          if y == names[x]
+            pigeon_list[names[x]][info] = []
+            pigeon_list[names[x]][info] << key.to_s
+            y += 1
+          else
+            y += 1
+          end
+        end
+      end
+    end
+    x += 1
+  end
+  
+  pigeon_list
 end
